db11fd5c774676ffd582525a7f3ccc0c9848ad48
GglLfr <glenn.folk08@gmail.com>
2026-07-08 16:18:19 +0700

    Use slab allocator for GDT

--- kernel/src/interrupt/x86_64.rs
+++ kernel/src/interrupt/x86_64.rs
@@ -1,6 +1,11 @@
 use core::{
     arch::{asm, naked_asm},
+    hint::{cold_path, spin_loop},
     mem::size_of_val_raw,
+    sync::atomic::{
+        AtomicU8,
+        Ordering::{Acquire, Relaxed, Release},
+    },
 };
 
 use bitflags::bitflags;
@@ -219,6 +224,28 @@ pub unsafe extern "sysv64" fn page_fault() -> ! {
 /// Only call this once per CPU core in setup phase after higher-half addressing is finished.
 pub unsafe fn init_interrupts() {
     static mut IDT_ENTRIES: [IdtEntry; 256] = [bytemuck::zeroed(); 256];
+    static IDT_STATE: AtomicU8 = AtomicU8::new(UNINIT);
+
+    const UNINIT: u8 = 0;
+    const LOCKED: u8 = 1;
+    const INIT: u8 = 2;
+
+    loop {
+        match IDT_STATE.compare_exchange_weak(UNINIT, LOCKED, Acquire, Relaxed) {
+            Ok(..) => unsafe {
+                cold_path();
+                IDT_ENTRIES[IdtIndex::DoubleFault as usize] = IdtEntry::new(double_fault);
+                IDT_ENTRIES[IdtIndex::PageFault as usize] = IdtEntry::new(page_fault);
+
+                IDT_STATE.store(INIT, Release);
+            },
+            Err(INIT) => break,
+            Err(..) => {
+                cold_path();
+                spin_loop();
+            }
+        }
+    }
 
     #[repr(C, packed)]
     struct IdtPointer {
@@ -229,9 +256,6 @@ pub unsafe fn init_interrupts() {
     unsafe {
         // Global descriptor table is x86-specific
         init_gdt();
-
-        IDT_ENTRIES[IdtIndex::DoubleFault as usize] = IdtEntry::new(double_fault);
-        IDT_ENTRIES[IdtIndex::PageFault as usize] = IdtEntry::new(page_fault);
 
         let ptr = IdtPointer {
             limit: u16::try_from(size_of_val_raw(&raw const IDT_ENTRIES) - 1).unwrap(),
